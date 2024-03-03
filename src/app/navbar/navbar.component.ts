dcb16c7fed4ad65b6e780e1caf65cf5dcfedc2ba
Lenovo <adnanhammoud002@gmail.com>
2024-03-03 10:03:26 +0100

    Highlighted menu item when selected done + added random users with random locations

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {MenuItem} from "primeng/api";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -8,30 +9,61 @@ import {MenuItem} from "primeng/api";
 })
 export class NavbarComponent {
   appTitle: string = 'ELECTRIGITY';
+  selectedMenuItem! : number;
+  style! : string;
 
+
+  constructor(private route: ActivatedRoute) {
+    this.route.url.subscribe(urlSegments => {
+      if (urlSegments.length > 0) {
+        const path = urlSegments[0].path;
+        if (path === 'home') {
+          this.selectedMenuItem = 1
+        } else if(path == 'search-options'){
+          this.selectedMenuItem = 2
+        } else if(path == 'operator') {
+          this.selectedMenuItem = 3
+        } else if(path == 'questions') {
+          this.selectedMenuItem = 4
+        }
+      }
+    });
+  }
+  ngOnInit() {
+    if(this.selectedMenuItem == 1) {
+      this.homeMenuItem.styleClass = 'widget-selected-menu'
+    } else if(this.selectedMenuItem == 2) {
+      this.searchMenuItem.styleClass = 'widget-selected-menu'
+    } else if(this.selectedMenuItem == 3) {
+      this.operatorMenuItem.styleClass = 'widget-selected-menu'
+    } else if(this.selectedMenuItem == 4) {
+      this.questionsMenuitem.styleClass = 'widget-selected-menu'
+    }
+  }
   homeMenuItem: MenuItem = {
     label: 'Home',
     icon: 'pi pi-home',
-    styleClass: 'widget',
-    routerLink: '/home'
+    routerLink: '/home',
+    styleClass: 'widget'
   }
 
   searchMenuItem: MenuItem = {
     label: 'Search',
     icon: 'pi pi-search',
-    styleClass: 'widget',
-    routerLink: '/search-options'
+    routerLink: '/search-options',
+    styleClass: 'widget'
   }
   operatorMenuItem: MenuItem = {
     label: 'Operator',
     icon: 'pi pi-phone',
-    styleClass: 'widget'
+    styleClass:  'widget'
   }
   questionsMenuitem: MenuItem = {
     label: 'Questions',
     icon: 'pi pi-question-circle',
     styleClass: 'widget'
   }
+
   items: MenuItem[] = [
     this.homeMenuItem,
     this.searchMenuItem,
